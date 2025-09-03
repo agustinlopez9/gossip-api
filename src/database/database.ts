d939fb98b0afab88a54413de4747b67e41c85181
agustinlopez9 <fernandopiceda99@gmail.com>
2025-09-03 16:40:15 -0300

    initial commit

---
+++ src/database/database.ts
@@ -0,0 +1,22 @@
+import type { Database } from 'types.ts'
+import { Pool } from 'pg'
+import { Kysely, PostgresDialect } from 'kysely'
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: "gossip_development",
+    host: "localhost",
+    user: "postgres",
+    password: "admin123",
+    port: 5432,
+    max: 10,
+  })
+})
+
+// Database interface is passed to Kysely's constructor, and from now on, Kysely 
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
+// to communicate with your database.
+export const db = new Kysely<Database>({
+  dialect,
+})
