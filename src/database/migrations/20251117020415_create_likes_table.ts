1df349161bdfe5b0da31f18b3c630449eca13cec
agustinlopez9 <fernandopiceda99@gmail.com>
2025-11-16 23:21:40 -0300

    create new tables

---
+++ src/database/migrations/20251117020415_create_likes_table.ts
@@ -0,0 +1,24 @@
+import { sql, type Kysely } from 'kysely'
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+	// up migration code goes here...
+	// note: up migrations are mandatory. you must implement this function.
+	// For more info, see: https://kysely.dev/docs/migrations
+	await db.schema.createTable('likes')
+		.addColumn('id', 'serial', (col) => col.primaryKey())
+		.addColumn('user_id', 'integer', (col) => col.notNull())
+		.addColumn('post_id', 'integer', (col) => col.notNull())
+		.addColumn('created_at', 'timestamp', (col) => col.notNull().defaultTo(sql`now()`))
+		.addForeignKeyConstraint('likes_user_id_fkey', ['user_id'], 'users', ['id'])
+		.addForeignKeyConstraint('likes_post_id_fkey', ['post_id'], 'posts', ['id'])
+		.execute();
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+	// down migration code goes here...
+	// note: down migrations are optional. you can safely delete this function.
+	// For more info, see: https://kysely.dev/docs/migrations
+	await db.schema.dropTable('likes').execute();
+}
